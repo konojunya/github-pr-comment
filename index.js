f72effeedea639ce7b58a0780ce379386ea7c8e4
konojunya <junya03dance@gmail.com>
2019-08-17 01:58:39 +0900

    a

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const Octokit = require("@octokit/rest");
-const axios = require("axios");
 const fs = require("fs");
 
 const octokit = new Octokit({
@@ -13,14 +12,16 @@ async function main() {
         .readFileSync(process.env.GITHUB_EVENT_PATH, { encoding: "utf-8" })
         .toString()
     );
-    const { pulls_url } = json.repository;
-    const res = await axios.get(pulls_url, {
-      headers: {
-        Authorization: `token ${process.env.GITHUB_ACCESS_TOKEN}`
-      }
-    });
+    const splited = json.repository.pulls_url.split(/\//);
+    const pull_request_number = splited[splited.length - 1];
+    const comment = await getComment(pull_request_number);
+    if (comment == null) {
+      createComment(pull_request_number);
+    } else {
+      updateComment(comment.id);
+    }
 
-    console.log(res);
+    console.log("done");
   } catch (e) {
     console.error(e);
     process.exit(1);
@@ -28,13 +29,32 @@ async function main() {
 }
 main();
 
-// async function createComment() {
-//   const res = await octokit.issues.createComment({
-//     owner: process.env.OWNER,
-//     repo: process.env.REPO,
-//     issue_number: await getIssueNumber(),
-//     body: "commented from api! https://github.com"
-//   });
+async function createComment(issue_number) {
+  const res = await octokit.issues.createComment({
+    owner: process.env.OWNER,
+    repo: process.env.REPO,
+    issue_number,
+    body: "commented from api! https://github.com"
+  });
 
-//   console.log(res);
-// }
+  console.log(res);
+}
+
+async function updateComment(comment_id) {
+  octokit.issues.updateComment({
+    owner: process.env.OWNER,
+    repo: process.env.REPO,
+    comment_id,
+    body: `updated!: ${new Date()}`
+  });
+}
+
+async function getComment(issue_number) {
+  const { data } = await octokit.issues.listComments({
+    owner: process.env.OWNER,
+    repo: process.env.REPO,
+    issue_number
+  });
+
+  return data.find(comment => comment.user.login === "konojunya");
+}
